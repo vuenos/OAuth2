97fcc95eefb00717f10ee7f6140c03790c6f6e5f
Jintae <vuenos@naver.com>
2025-06-02 15:59:27 +0900

    개발환경설정 및 react-router-dom 인스톨

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const handleOAuthGoogle = () => {
+        console.log('Google OAuth');
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <h1>OAuth2 demo</h1>
+
+                <div>
+                    <button type="button" onClick={handleOAuthGoogle}>
+                        Google
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
